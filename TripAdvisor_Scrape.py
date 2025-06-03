afdfa67f8729fd5430a845ff9e9797a87b8ebc78
mattmeist <122904694+mattmeist@users.noreply.github.com>
2025-06-03 10:24:37 -0700

    Update TripAdvisor_Scrape.py

--- TripAdvisor_Scrape.py
+++ TripAdvisor_Scrape.py
@@ -17,13 +17,11 @@ import pandas as pd
 import re # Regular expressions, to help us parse html, json.
 import requests # To pull internet pages
 import time # To allow us to "sleep" the scraping, so as not to overwhelm the servers
+from datetime import datetime, timedelta
 import os # To create folders
 import math
 from bs4 import BeautifulSoup # To make html easier to parse
 
-# Need selenium to get the list of attractions and first page of each attraction
-# I don't like it either. If you need, you will have to find a chromedriver online
-# Google it
 import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -34,9 +32,11 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--start-maximized")
 
 # Path to ChromeDriver
-chrome_driver_path = "YOURPATH/chromedriver" 
+chrome_driver_path = "chromedriver-mac-arm64/chromedriver" 
+
 service = Service(chrome_driver_path)
 chrome_options.add_argument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
 
@@ -47,11 +47,6 @@ def create_directory(directory):
         print(f"Directory '{directory}' created.")
     else:
         print(f"Directory '{directory}' already exists.")
-
-# Create directories if they don't exist
-create_directory(f"Attractions_{city}")
-create_directory(f"Attractions_{city}_Questions")
-create_directory(f"Attractions_{city}_Answers")
 
 # Flatten dictionaries function
 def flatten_dict(d, parent_key='', sep='_'):
@@ -64,16 +59,24 @@ def flatten_dict(d, parent_key='', sep='_'):
             items.append((new_key, v))
     return dict(items)
 
-# Function to fetch reviews.
-# This is a bit of brute force. This is written so that it will definitely get 
-# any page that is available, but may take forever to do so. You can change that
-# by turning down (or up??) the `attempts` argument.
+# Create directories if they don't exist
+create_directory(f"Attractions_{city}")
+create_directory(f"Attractions_{city}_Reviews")
+create_directory(f"Attractions_{city}_Questions")
+create_directory(f"Attractions_{city}_Answers")
+
 # Function to find links for tokens
 def find_links_index(content):
     for idx, item in enumerate(content):
         if 'links' in item:
             return idx
     raise ValueError("No item with a 'links' key was found.")
+    
+# Function to fetch reviews.
+# This is a bit of brute force. This is written so that it will definitely get 
+# any page that is available, but may take forever to do so. You can change that
+# by turning down (or up??) the `attempts` argument.
+
     
 #Create review fetch function
 def fetch_reviews(page, geoId, detailId, attraction, token, url, relative_url):
@@ -422,15 +425,27 @@ for index, row in attractions.iterrows():
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, "html.parser")
     pages = list(range(math.ceil(int(re.search(r'of\s([\d,]+)', soup.find('section', id='REVIEWS').find('div', class_='Ci').text).group(1).replace(',', ''))/10)))
-    
-    # Now get reviews
-    
+    relative_url = f"/Attraction_Review-g{geoId}-d{detailId}-Reviews-{attraction}-{city}.html"
+    max_qs = int(re.search("([\d,]+)\sq",soup.find('section', id='REVIEWS').find("div", id="tab-qa-content").find('span', class_="biGQs _P XWJSj Wb").text)[1])
+    runtime = math.ceil((len(pages)*3.6 + 
+                         math.ceil(max_qs/50*2) + 
+                         math.ceil(max_qs*2))/60/6)/10
+    reviews_runtime = math.ceil((len(pages)*3.6)/60/6)/10
+    questions_runtime = round((max_qs/50*2.6)/60/60,5)
+    answers_runtime = math.ceil((math.ceil(max_qs*2.6))/60)/60
+    starttime = datetime.now().strftime('%I:%M %p')
+    finishtime = (datetime.now() + timedelta(hours=runtime)).strftime('%I:%M %p')
+    print(f"Scraping {attraction}: \n Starting at {starttime}. Will take ~{runtime} hours.")
+    print(f"\tReviews: {reviews_runtime} hours; {(datetime.now() + timedelta(hours=reviews_runtime)).strftime('%I:%M %p')}.")
+    print(f"\tQuestions: {math.ceil(questions_runtime*3600)} seconds.")
+    print(f"\tAnswers: {math.ceil(answers_runtime*60)} minutes; {(datetime.now() + timedelta(hours=questions_runtime+reviews_runtime+answers_runtime)).strftime('%I:%M %p')}.")
+
     all_reviews = []
-    
+    start_url = f"/Attraction_Review-g{geoId}-d{detailId}-Reviews-{attraction}-{city}.html"
     for page in pages:
         if page == 0:
             #Get next page
-            token = re.search("updateToken%5C%5C%5C%22%3A%5C%5C%5C%22(.*?)%5C%5C%5C%22%7D%7D%2C%7B%5C%5C%5C%22", page_source)[1]
+            token = re.search("updateToken%5C%5C%5C%22%3A%5C%5C%5C%22(.*?)%5C%5C%5C%22", page_source)[1]
             relative_url = f"/Attraction_Review-g{geoId}-d{detailId}-Reviews-or{(page+1)*10}-{attraction}-{city}.html"
 
         else:
@@ -438,8 +453,16 @@ for index, row in attractions.iterrows():
             relative_url = f"/Attraction_Review-g{geoId}-d{detailId}-Reviews-or{offset}-{attraction}-{city}.html"
 
             #Get reviews
-            reviews, links = fetch_reviews(page, geoId, detailId, attraction, token, url, relative_url)
-            all_reviews.extend(reviews)  # Extend adds all rows (a list of dictionaries) to the list
+            for attempt in range(10):
+                try:
+                    reviews, links = fetch_reviews(page, geoId, detailId, attraction, token, url, relative_url)
+                    all_reviews.extend(reviews)  # Extend adds all rows (a list of dictionaries) to the list
+                    break
+                except (requests.exceptions.RequestException, KeyError, IndexError) as e:
+                    if attempt < 10 - 1:  # Retry until the last attempt
+                        time.sleep(10)
+                    else:
+                        raise RuntimeError(f"Failed after multiple attempts: {e}")
 
             # Collect back token when this is second page, collect prior page
             if page == 1:
@@ -452,22 +475,49 @@ for index, row in attractions.iterrows():
             else:
                 token = links['links'][2]['updateLink']['updateToken']
 
-        time.sleep(4)
+        time.sleep(3)
 
         if page % 50 == 0:
             df = pd.DataFrame(all_reviews).drop_duplicates()
-            df.to_csv(f'Attractions_{city}/{attraction}.csv', index=False)
+            df.to_csv(f'Attractions_{city}_Reviews/{attraction}.csv', index=False)
+
     df = pd.DataFrame(all_reviews).drop_duplicates()
-    df.to_csv(f'Attractions_{city}/{attraction}.csv', index=False)
+    df.to_csv(f'Attractions_{city}_Reviews/{attraction}.csv', index=False)
+    print(f"\n Reviews finished at {datetime.now().strftime('%I:%M %p')}. \n\tN = {len(df):,}.")
     
-    # Find questions
-    relative_url = f"/Attraction_Review-g{geoId}-d{detailId}-Reviews-{attraction}-{city}.html"
     limit = 50
-    max_qs = int(re.search("([\d,]+)\sq",html.find('section', id='REVIEWS').find("div", id="tab-qa-content").find('span', class_="biGQs _P XWJSj Wb").text)[1])
-    questions_df = pd.DataFrame(fetch_questions(geoId, detailId, attraction, max_qs, limit, url, headers, relative_url)).drop_duplicates()
-    questions_df.to_csv(f'Attractions_{city}_Questions/{attraction}.csv', index=False)
-    
-    # Find Answers
-    answers_df = pd.DataFrame(fetch_answers(geoId, detailId, attraction, questions_df, headers, url, relative_url)).drop_duplicates()
-    answers_df.to_csv(f'Attractions_{city}_Answers/{attraction}.csv', index=False)
+    relative_url = f"/Attraction_Review-g{geoId}-d{detailId}-Reviews-{attraction}-{city}.html"
+    # Define headers
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "*/*",
+        "Sec-Fetch-Site": "same-origin",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Host": "www.tripadvisor.com",
+        "Origin": "https://www.tripadvisor.com",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0.1 Safari/605.1.15",
+        "Referer": f"https://www.tripadvisor.com{relative_url}",
+        "Connection": "keep-alive",
+        "Sec-Fetch-Dest": "empty"
+    }
+    if i < 2:
+        questions_df = pd.DataFrame(fetch_questions(geoId, detailId, attraction, max_qs, limit, url, headers, relative_url)).drop_duplicates()
+        questions_df.to_csv(f'Attractions_{city}_Questions/{attraction}.csv', index=False)
+        print(f" Questions finished at {datetime.now().strftime('%I:%M %p')}. \n\tN = {len(questions_df):,}.")
+        answers_df = pd.DataFrame(fetch_answers(geoId, detailId, attraction, questions_df, headers, url, relative_url)).drop_duplicates()
+        answers_df.to_csv(f'Attractions_{city}_Answers/{attraction}.csv', index=False) 
+        print(f" Answers finished at {datetime.now().strftime('%I:%M %p')}. \n\tN = {len(answers_df):,}.")
+        i += 1
+    elif i > 3:
+        questions_df = pd.DataFrame(fetch_questions(geoId, detailId, attraction, max_qs, limit, url, headers, relative_url)).drop_duplicates()
+        questions_df.to_csv(f'Attractions_{city}_Questions/{attraction}.csv', index=False)
+        print(f" Questions finished at {datetime.now().strftime('%I:%M %p')}. \n\tN = {len(questions_df):,}.")
+        answers_df = pd.DataFrame(fetch_answers(geoId, detailId, attraction, questions_df, headers, url, relative_url)).drop_duplicates()
+        answers_df.to_csv(f'Attractions_{city}_Answers/{attraction}.csv', index=False) 
+        print(f" Answers finished at {datetime.now().strftime('%I:%M %p')}. \n\tN = {len(answers_df):,}.")
+        i += 1
+    else:
+        i += 1
+        next
                
